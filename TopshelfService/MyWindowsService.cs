cd7281152c277eade02390a0e3fb44c709d4856c
vinh pham <vinh.pham@niteco.se>
2019-05-08 17:53:29 +0700

    TopshelfService

---
+++ TopshelfService/MyWindowsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Timers;
+
+namespace TopshelfService
+{
+    public class MyWindowsService
+    {
+        readonly Timer _timer;
+        public void Start()
+        {
+            Console.WriteLine("Start");
+        }
+
+        public MyWindowsService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerOnElapsed;
+            _timer.Start();
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+            Console.WriteLine("Stop");
+        }
+    }
+}
---
+++ TopshelfService/Program.cs
@@ -0,0 +1,24 @@
+﻿using Topshelf;
+
+namespace TopshelfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                hostConfig.Service<MyWindowsService>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new MyWindowsService());                    
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+                });
+                hostConfig.RunAsLocalSystem();
+                hostConfig.SetDescription("Sample Topshelf Host");                   //7
+                hostConfig.SetDisplayName("TopshelfService");                                  //8
+                hostConfig.SetServiceName("TopshelfService");
+            });
+        }
+    }
+}
