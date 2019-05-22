f5c890a51183225200c184cc6809190416f08cbd
vinh pham <vinh.pham@niteco.se>
2019-05-22 11:58:15 +0700

    implement workflow

---
+++ WorkflowConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+
+namespace WorkflowConsoleApplication
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Activity wf = new WriteLine
+            //{
+            //    Text = "Hello World."
+            //};
+
+            //WorkflowInvoker.Invoke(wf);
+
+            //Console.WriteLine("Input age1: ");
+            //var checkName = new CheckName();
+            //checkName.Age = Convert.ToInt32(Console.ReadLine());
+            //WorkflowInvoker.Invoke(checkName);
+            //Console.ReadKey();
+
+            Console.Write("Input age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            WorkflowInvoker.Invoke(new ConfirmWF(),
+                new Dictionary<string, object>()
+                {
+                    { "Age", age }
+                });
+
+        }
+    }
+}
