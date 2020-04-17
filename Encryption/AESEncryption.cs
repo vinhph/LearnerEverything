204f06ef421ace43dca5b2515db9203a181e3155
NITECO\vinh.pham <vinh.pham@niteco.se>
2020-04-17 10:16:30 +0700

    Encryption

---
+++ Encryption/AESEncryption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    public class AESEncryption
+    {
+        /// <summary>
+        /// Generate a private key
+        /// From : www.chapleau.info/blog/2011/01/06/usingsimplestringkeywithaes256encryptioninc.html
+        /// </summary>
+        public static string GenerateKey(int iKeySize)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = iKeySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.GenerateIV();
+            string ivStr = Convert.ToBase64String(aesEncryption.IV);
+            aesEncryption.GenerateKey();
+            string keyStr = Convert.ToBase64String(aesEncryption.Key);
+            string completeKey = ivStr + "," + keyStr;
+
+            return Convert.ToBase64String(ASCIIEncoding.UTF8.GetBytes(completeKey));
+        }
+
+        /// <summary>
+        /// Encrypt
+        /// From : www.chapleau.info/blog/2011/01/06/usingsimplestringkeywithaes256encryptioninc.html
+        /// </summary>
+        public static string Encrypt(string iPlainStr, string iCompleteEncodedKey, int iKeySize)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = iKeySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.IV = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(iCompleteEncodedKey)).Split(',')[0]);
+            aesEncryption.Key = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(iCompleteEncodedKey)).Split(',')[1]);
+            byte[] plainText = ASCIIEncoding.UTF8.GetBytes(iPlainStr);
+            ICryptoTransform crypto = aesEncryption.CreateEncryptor();
+            byte[] cipherText = crypto.TransformFinalBlock(plainText, 0, plainText.Length);
+            return Convert.ToBase64String(cipherText);
+        }
+
+        /// <summary>
+        /// Decrypt
+        /// From : www.chapleau.info/blog/2011/01/06/usingsimplestringkeywithaes256encryptioninc.html
+        /// </summary>
+        public static string Decrypt(string iEncryptedText, string iCompleteEncodedKey, int iKeySize)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = iKeySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.IV = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(iCompleteEncodedKey)).Split(',')[0]);
+            aesEncryption.Key = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(iCompleteEncodedKey)).Split(',')[1]);
+            ICryptoTransform decrypto = aesEncryption.CreateDecryptor();
+            byte[] encryptedBytes = Convert.FromBase64CharArray(iEncryptedText.ToCharArray(), 0, iEncryptedText.Length);
+            return ASCIIEncoding.UTF8.GetString(decrypto.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length));
+        }
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainStr = "66271e81-60a0-493b-baaa-4a09c1ba2c65";
+            Console.WriteLine("Original Text: {0}", plainStr);
+            int keySize = 256;
+            string completeEncodedKey = "d1U5SEFjYlV0Q3J5dUtHUVRKYlhrQT09LGoycExrQzhnR0U4V0V4VStnN25NbE82TWFuNlpEVVNsY1NuQ2JvN2F6WjQ9"; //AESEncryption.GenerateKey(keySize);
+            Console.WriteLine("Using complete key '{0}'", completeEncodedKey);
+            string encryptedText = AESEncryption.Encrypt(plainStr, completeEncodedKey, keySize);
+            Console.WriteLine("Encrypted Text: {0}", encryptedText);
+            encryptedText = Base64Encryption.Base64Encode(encryptedText);
+            Console.WriteLine("Encode Base64 Text: {0}", encryptedText);
+            string decrypted = Base64Encryption.Base64Decode(encryptedText);
+            Console.WriteLine("Decode Base64 Text: {0}", decrypted);
+            decrypted = AESEncryption.Decrypt(decrypted, completeEncodedKey, keySize);
+            Console.WriteLine("Decrypted Text: {0}", decrypted);
+            Console.Read();
+        }
+    }
+}
